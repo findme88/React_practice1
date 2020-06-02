b13f8d465fd61a8c9d74637a8fe0c7ccbc1261e9
zordexxx <zordexxx69@gmail.com>
2020-06-02 15:07:01 +0300

    start

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -2,32 +2,41 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const styles = {
+  li: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginBottom: ".5rem",
+  },
 
-li: {
-display: 'flex',
-justifyContent: 'space-beetween',
-alignItems: 'center',
-padding: '.5rem 1rem',
-border: '1px solid #ccc',
-borderRadius: '4px',
-marginBottom: '.5rem'
-},
+  input: {
+    marginRight: "1rem",
+  },
+};
 
-input: {
-  marginRight: '1rem'
-}
+function TodoItem({ todo, index, onChange }) {
+  const classes =[]
 
-}
-
-function TodoItem({ todo, index }) {
+  if  (todo.completed) {
+    classes.push('done')
+  }
   return (
     <li style={styles.li}>
-      <span>
-        <input type="checkbox" />
-      <strong>{index + 1}</strong>
-      {todo.title}
+      <span className={classes.join(' ')}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          style={styles.input}
+          onChange={() => onChange(todo.id)}
+        />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
       </span>
-      <button>&times;</button>
+      <button className="rm">&times;</button>
     </li>
   );
 }
@@ -35,6 +44,7 @@ function TodoItem({ todo, index }) {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
   index: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
