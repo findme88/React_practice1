a75ebe336a849534eccee7986ab873f28114a34c
zordexxx <zordexxx69@gmail.com>
2020-06-02 17:51:53 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: 'Buy bread'},
-    { id: 2, completed: false, title: 'Buy butter'},
-    { id: 3, completed: false, title: 'Buy milk'}
-  ])
-  
-function toggleTodo(id) {
-  setTodos(
-    todos.map(todo => {
-    if (todo.id === id) {
-      todo.completed = !todo.completed
-    }
-    return todo
-  })
-  )
-}
+    { id: 1, completed: false, title: "Buy bread" },
+    { id: 2, completed: false, title: "Buy butter" },
+    { id: 3, completed: false, title: "Buy milk" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
 
   return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
-
-      <TodoList todos={todos} onToggle={toggleTodo} />
-    </div>
+    <Context.Provider value={{ removeTodo: removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>no todos</p>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import Context from '../context'
+
 
 const styles = {
   li: {
@@ -18,6 +20,7 @@ const styles = {
 };
 
 function TodoItem({ todo, index, onChange }) {
+  const {removeTodo} = useContext(Context)
   const classes =[]
 
   if  (todo.completed) {
@@ -36,7 +39,7 @@ function TodoItem({ todo, index, onChange }) {
         &nbsp;
         {todo.title}
       </span>
-      <button className="rm">&times;</button>
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)}>&times;</button>
     </li>
   );
 }
